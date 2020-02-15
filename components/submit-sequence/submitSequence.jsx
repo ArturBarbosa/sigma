8fd3be9a7b1c202485a307e113285b274e906b06
Artur Barbosa <artur@Arturs-MacBook-Pro.local>
2020-02-15 13:12:27 -0800

    first API being served, still no database

---
+++ components/submit-sequence/submitSequence.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './submitSequence.css';
+
+/**
+ * Define TopBar, a React componment of CS142 project #5
+ */
+class SubmitSequence extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Hello, here should be the form!
+      </div>
+    );
+  }
+}
+
+export default SubmitSequence;
---
+++ sigma.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  HashRouter, Route, Switch
+} from 'react-router-dom';
+import './styles/main.css';
+
+// import necessary components
+import SubmitSequence from './components/submit-sequence/SubmitSequence';
+
+class Sigma extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <HashRouter>
+      <div>
+            <Switch>
+            <Route exact path="/" component={SubmitSequence}/>
+            </Switch>
+      </div>
+    </HashRouter>
+    );
+  }
+}
+
+
+ReactDOM.render(
+  <Sigma />,
+  document.getElementById('root'),
+);
